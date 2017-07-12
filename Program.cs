00d100cd5623a23bd394247811847c248435a721
gurma <gurma@LAPTOP-RUMP0R02>
2017-07-12 16:13:42 -0400

    Modified Program.cs

--- Program.cs
+++ Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name: Gurman Mangat
+ * Date: July 11,2017
+ * DEscription: Demo for Lesson 9
+ * Version: 0.1 - Created the Program class
+ */
+ 
 namespace COMP_123_S2017_Lesson_09
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
