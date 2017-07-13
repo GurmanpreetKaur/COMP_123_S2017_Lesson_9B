7ac53a97f98aa85c373150cc3d6fec48d7b75a99
gurma <gurma@LAPTOP-RUMP0R02>
2017-07-13 13:22:43 -0400

    added IHasKarma Interface

---
+++ IHasKarma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Gurmanpreet Kaur
+ * Date: July 13,2017
+ * DEscription: this is the interface that defines a karma property that must be implemented in any class that subscribes  
+ * Version: 0.3  created IHasKarma*/
+namespace COMP_123_S2017_Lesson_09
+{
+    /// <summary>
+    /// this is the IHasKarma interface
+    /// </summary>
+    public interface IHasKarma
+    {
+
+        //public properties
+        int Karma { get; set; }
+    }
+}
