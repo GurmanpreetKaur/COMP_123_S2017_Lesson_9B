eef7f6604e3e625f510779dad123df69a45eec88
gurma <gurma@LAPTOP-RUMP0R02>
2017-07-13 12:59:41 -0400

    used the ToString method of the SuperHuman class

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
  * DEscription: Demo for Lesson 9
  * Version: 0.3  - Added another Power to the SuperHuman object
  *               - Also Displayed all the powers in the Powers List
+ *               - used the ToString methos of the SuperHuman class 
  */
  
 namespace COMP_123_S2017_Lesson_09
@@ -18,9 +19,13 @@ namespace COMP_123_S2017_Lesson_09
         static void Main(string[] args)
         {
             SuperHuman superhuman = new SuperHuman("Super dude");
-            superhuman.AddPower("Spider Climbing", 50);
+            superhuman.Powers.Add(new Power("Teleportation ", 50));
+            superhuman.Powers.Add(new Power("Phasing", 40));
+            superhuman.Powers.Add(new Power("Invisibility Rank ", 30));
+            Console.WriteLine(superhuman.ToString());
+           /* superhuman.AddPower("Spider Climbing", 50);
             superhuman.AddPower("Webcasting", 40);
-            superhuman.DisplayPowers();
+            superhuman.DisplayPowers();*/
         }
     }
 }
