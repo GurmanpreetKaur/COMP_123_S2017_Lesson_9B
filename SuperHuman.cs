4546abd542668b1be05e23530ab6d07d3d2efb15
gurma <gurma@LAPTOP-RUMP0R02>
2017-07-12 20:46:37 -0400

    Addes SuperHuman Class

---
+++ SuperHuman.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+* Name: Gurman Mangat
+* Date: July 11,2017
+* DEscription: This is the SuperHuman class that extends the Human Abstract class
+* Version: 0.1 - Created the SuperHuman class
+*/
+namespace COMP_123_S2017_Lesson_09
+{
+    /// <summary>
+    /// This is the SuperHuman class
+    /// </summary>
+   
+    public class SuperHuman : Human
+    {
+        // private instance variables
+
+        // public properties
+
+        // constructors
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+
+        //private methods
+
+        //public methods
+    }
+}
